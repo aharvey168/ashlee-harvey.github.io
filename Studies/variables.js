7fef9fd5713f3d007f69d78b3c930c32c253b1c6
aharvey168 <Ashlee.j.harvey@gmail.com>
2022-05-03 02:36:41 +0000

    recursion

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,15 +1,36 @@
 /**
 VARIABLES
 
-0. USE: To hold things in memor yduring the life-cycle of a program, we can use variables. 
+0. USE: To hold things in memory during the life-cycle of a program, we can use variables. 
+
+
 Variable are names indentifiers that can point to values ofa particular type.
 
 1. Variable types: number, string, boolean, array, object 
 
-2. 2 Phases of USE: declaration and initialization
+2. TWO Phases of USE: declaration and initialization
+    Declaration : TO create variable we use the keyword follwed by a name for the variable name
+        - key word(let, var, const) 
+        - Examples:
+            var		age
+            let		name		
+            const	amAnAdult
+    Initialization: assingning a value to the variable by place the assignment operator follows by the variable n
+                name
+            Examples:
+            =		28;
+            =		“Ashlee”;	
+            =	 	true;
 
-3 .TO create variable we use the keyword, var, follwed by a name for the variable
+3. Data Types
+    Primitive 
+        String 
+        Number 
+        Boolean
+    Complex
+        Object
+        Array
+        Function
 
-4. To declare a variable
-
+    
  */
