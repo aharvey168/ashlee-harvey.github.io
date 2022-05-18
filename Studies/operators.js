942c73a79c8745c9c612b0eac36696d9bf232ce5
aharvey168 <Ashlee.j.harvey@gmail.com>
2022-05-18 01:53:49 +0000

    studies

--- Studies/operators.js
+++ Studies/operators.js
@@ -1,5 +1,5 @@
-/**
- * OPERATORS
+
+ /* OPERATORS
  * 
  * OBJECTIVES:
  * - operators act on our data and perform arithmetic operations on them
@@ -7,27 +7,53 @@
  *      1. Unary - work with 1 value
  *      2. Binary - work with 2 values
  *      3. Ternary - work with 3 values
- * 
- * TYPES
- * 1. Assignment
- * 2. Comparision
- *    - compare twp values and evaluate to true or false
- *    - binary comparison: <, >, <=, >=
- *    - strict comparison: ===, !==
- * 3. Arithmetic
- *    - math function that performs a calculation on two operands
- *    - (+) Addition
- *    - (-) Subtraction
- *    - (*) Multiplication
- *    - (/) Division
- * 4. Logical
- *    - && And Operator
- *    - || OR Operator
- *    - ! Bang Operator
- * 5. String
- *      - ("") concat operator -returns a concat version of two strings
- * 6. Conditional
- * 7. Unary
- *    - +, -, ++, --, !, ~
- * 
  */
+
+
+  //TYPES//
+  //Assignment
+  
+  //Comparision compare twp values and evaluate to true or false
+     //binary comparison (< > <= >= )
+     // strict comparison: ===, !==
+  //Arithmetic - math function that performs a calculation on two operands
+     2 + 3 //Addition
+     2 - 3//Subtraction
+     2 *  3//Multiplication
+     2 / 3 // Division
+  // Logical
+      && //And Operator
+      || //OR Operator
+      ! //Bang Operator
+  //Assignment = assign values to variable
+      let x = y //assigns a value
+      x += y //adds a value to a variable
+      x -= y //subtracts a value from a variable
+      x *= y //multiplies a variable
+      x *= y //divides a variable
+  /*Ternary(Conditional) - the 3 operands it takes in: condition (?) truthy expression
+  (:), and falsy expression
+  */
+      var beverage = (age >= 21) ? "Beer" : "Juice";   
+  //Unary - 1 operand or one value is being worked on
+     + //convert the operand into a number
+     +3 //returns 3 
+
+     - // convert the operand into a number and becomes negative
+     -3 //returns -3 =>
+
+     ++ // Adds one to its operand
+     x = 4
+     y = x++ //returns y = 4 and  x = 5 =>
+
+     -- //Decrements by one from its operand
+     x = 4
+     y = x-- //returns y = 4 and  x = 3  
+
+     !  //=> Converts to boolean value then negates it
+     !false //returns true
+
+     typeOf //=> returns a string which is the type of the operand
+     typeOf 3 //returns "number"
+  
+ 
--- Studies/variables.js
+++ Studies/variables.js
@@ -1,36 +1,37 @@
 /**
-VARIABLES
+ * VARIABLES
+ * 0. USE: To hold things in memory during the life-cycle of a program, we can use variables
+ * 
+ * 1. Variable are named indentifiers that can point to values ofa particular type.
+ * 
+ * 2. Variable types: number, string, boolean, array, object
+ * 
+ * 3. TWO Phases of USE: declaration and initialization
+ */
 
-0. USE: To hold things in memory during the life-cycle of a program, we can use variables. 
+//1. Declaration//
+ // TO create variable we use the keyword follwed by a name for the variable name. key to use words: (let, var, const)
+    //Examples:
+            var	age; //global scope
+            let	named; //block scope local variable
+            const anAdult; //block scope
+    
+//2. Initialization//
+// assingning a value to the variable by place the assignment operator follows by the variable n
+    //Examples:
+    var	age = 28;
+    let	name = Ashlee;	
+    const amAnAdult = true;
 
-
-Variable are names indentifiers that can point to values ofa particular type.
-
-1. Variable types: number, string, boolean, array, object 
-
-2. TWO Phases of USE: declaration and initialization
-    Declaration : TO create variable we use the keyword follwed by a name for the variable name
-        - key word(let, var, const) 
-        - Examples:
-            var		age
-            let		name		
-            const	amAnAdult
-    Initialization: assingning a value to the variable by place the assignment operator follows by the variable n
-                name
-            Examples:
-            =		28;
-            =		“Ashlee”;	
-            =	 	true;
-
-3. Data Types
-    Primitive 
-        String 
-        Number 
-        Boolean
-    Complex
-        Object
-        Array
-        Function
+//3. Data Types//
+//Primitive 
+       var message = "Can I help you?" //String 
+        var sum = 9; //Number 
+        var isTrue = false; //Boolean
+//Complex
+        var person = { name: "David"};  //Object
+        var array = [1, 2, 3, 4]; //Array
+        var arrowSum = (x, y) => (x + y); //Function
 
     
- */
+ 
